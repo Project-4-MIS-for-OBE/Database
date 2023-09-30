54f0811a8ce3398121a845a2c877e063410f9e21
panthaweekan_somngam <tatanoobza1234@hotmail.com>
2023-09-30 19:27:47 +0700

    Update CourseModel validation

--- models/Course.js
+++ models/Course.js
@@ -1,27 +1,50 @@
 const mongoose = require('mongoose');
 
-const courseSchema = new mongoose.Schema(
-    {
-        courseNo: {
-            type : String,
-            required : 'Course can not be blank'},
-        year : String,
-        semester : String,
-        section : {
-            type : String,
-            required : 'Course can not be blank'},
-        csoList : {
-          type : Array,
-          objEN: String,
-          objTH: String,
-          selectedSO: [Number],
-          csoScore: {
-            type : Number,
-            default : 0,
-            required : true
-          }
+const courseSchema = new mongoose.Schema({
+    courseNo: {
+        type : String,
+        required : true
+    },
+    year : {
+        type : String,
+        required : true,
+        unique: true
+    },
+    semester : {
+        type : String,
+        required : true,
+        unique: true
+    },
+    section : [{
+        sectionNumber: {
+            type: String,
+            required: true
+        },
+        csoList: {
+            type: Array,
+            objEN: String,
+            objTH: String,
+            selectedSO: [Number],
+            csoScore: {
+                type: Number,
+                default: 0,
+                required: true
+            }
         }
-      },
-    { collection: 'courses' })
+    }]
+}, { collection: 'courses' });
 
-    module.exports = mongoose.model('courses', courseSchema);
+courseSchema.pre('validate', function(next) {
+    const sections = this.section.map(sec => sec.sectionNumber);
+    const uniqueSections = new Set(sections);
+
+    if (sections.length !== uniqueSections.size) {
+        return next(new Error('Sections must be unique within a course.'));
+    }
+
+    next();
+});
+
+courseSchema.index({ year: 1, semester: 1}, { unique: true });
+
+module.exports = mongoose.model('courses', courseSchema);
