366841ee7fa1ba8d1a6bc041da272740716555f7
earth <konthakarn_f@cmu.ac.th>
2023-10-03 22:54:01 +0700

    can see relevantCourse

--- routes/getData.js
+++ routes/getData.js
@@ -138,7 +138,7 @@ router.get('/test', async (req, res) => {
     // Initialize an array to store sections data
 
     const sectionsData = [];
-
+    const relevantCourse = [];
     // Iterate over each course number
     for (let i = 0; i < courseNoList.length; i++) {
 
@@ -175,6 +175,15 @@ router.get('/test', async (req, res) => {
             coTeachers: coTeachers.length > 0 ? coTeacherNames : null,
           };
         } else {
+          const relevant = {
+            courseNo: courseNo,
+            section: [section.section],
+            courseTitleTH: courseTitleTH,
+            courseTitleEN: courseTitleEN,
+            instructorName: section.teacher.firstNameEN + " " + section.teacher.lastNameEN,
+            coTeachers: coTeachers.length > 0 ? coTeacherNames : null
+          };
+          relevantCourse.push(relevant);
           return null; // Skip this section if the condition is not met
         }
       });
@@ -199,83 +208,7 @@ router.get('/test', async (req, res) => {
       });
 
 
-      const courses = await Course.find({ courseNo: '261336', year: '2566', semester: '5' }).exec();
-      const a = {
-        courseNo: "261336",
-        year: "2566",
-        semester: "5",
-        section: [
-          {
-            sectionNumber: "1",
-            status: "In Progress",
-            csoList: [
-              {
-                objEN: "Able to describe how web application technology works",
-                objTH: "อธิบายหลักการทำงานของเว็บแอพพลิเคชันและการสื่อสารที่เกี่ยวข้อง",
-                selectedSO: [7],
-                csoScore: 1
-              },
-              {
-                objEN: "Able to develop a web application using software framework",
-                objTH: "พัฒนาเว็บแอพพลิเคชันด้วยเฟรมเวิร์คด้านซอฟต์แวร์ได้",
-                selectedSO: [5, 1],
-                csoScore: 2
-              },
-              {
-                objEN: "Able to develop data service using software framework",
-                objTH: "พัฒนาบริการข้อมูลเบื้องหลังด้วยเฟรมเวิร์คด้านซอฟต์แวร์ได้",
-                selectedSO: [1, 5],
-                csoScore: 3
-              }
-            ]
-          },
-          {
-            sectionNumber: "2",
-            status: "In Progress",
-            csoList: [
-              {
-                objEN: "Example CSO for section 802",
-                "objTH": "ตัวอย่าง CSO สำหรับหัวข้อ 802",
-                selectedSO: [3],
-                csoScore: 2
-              }
-            ]
-          }
-        ]
-      }
-      if (courses.length > 0) {
-        // At least one course found
-        const sectionNumbers = courses[0].section.map(section => section.sectionNumber);
-        const allstatus = courses[0].section.map(section => section.status);
-        let status = "fail"; // Default status if no sectionNumber equals "1"
-
-        for (let i = 0; i < courses[0].section.length; i++) {
-          if (sectionNumbers[i] === "3") {
-            status = allstatus[i];
-            break; // Found a section with sectionNumber === "1", exit the loop
-          }
-        }
-
-        if (status == "fail") {
-          (courses[0].section.push({
-            sectionNumber: "3", status: "In Progress", csoList: [
-              {
-                objEN: "Example CSO for section 802",
-                "objTH": "ตัวอย่าง CSO สำหรับหัวข้อ 802",
-                selectedSO: [3],
-                csoScore: 2
-              }
-            ]
-          }))
-          status = "In Progress";
-          await courses[0].save();
-        }
-        res.status(200).json(status);
-      } else {
-        // No courses found
-        (await Course.create(a)).save();
-        res.status(200).json(a);
-      }
+     
 
 
 
@@ -288,7 +221,7 @@ router.get('/test', async (req, res) => {
 
     sectionsData.sort((a, b) => a.courseNo.localeCompare(b.courseNo));
 
-    res.status(200).json({ instructorName, sectionsData });
+    res.status(200).json({ instructorName, sectionsData,relevantCourse });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
