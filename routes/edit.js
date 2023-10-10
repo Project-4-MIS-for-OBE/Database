816906c9abfeba295e2fd2ea41096bc481580de0
Heart <supalit1@gmail.com>
2023-10-10 19:07:15 +0700

    update

--- routes/edit.js
+++ routes/edit.js
@@ -28,193 +28,119 @@ router.get('/', async (req, res) => {
 
     // Fetch course data from the first API endpoint
     const response1 = await axios.get('https://qa.cpe.eng.cmu.ac.th/api/3rdParty/course?semester=' + `${semester}` + '&year=' + `${year}`, config_1);
-    
+
     // Filter courses where coordinatorCmuAccount matches the specified email
     // const coursesFilteredByCoordinator = response1.data.courses.filter((courseno) => {
     //   return courseno.courseNo === courseNo;
     // });
     let curriculum = null;
-    if(Number(section) >700 && Number(section)<=800){
-         curriculum = 'ISNE';
-    }else{
-         curriculum = 'CPE';
+    if (Number(section) > 700 && Number(section) <= 800) {
+      curriculum = 'ISNE';
+    } else {
+      curriculum = 'CPE';
     }
-    console.log(curriculum);
-     const response2 = await axios.get('https://qa.cpe.eng.cmu.ac.th/api/3rdParty/so?curriculum=' + `${curriculum}` + '&year=' + `${year}`, config_1);
-     const soList = response2.data.so;
+    const response2 = await axios.get('https://qa.cpe.eng.cmu.ac.th/api/3rdParty/so?curriculum=' + `${curriculum}` + '&year=' + `${year}`, config_1);
+    const soList = response2.data.so;
 
-     const coursesFilteredBycourseNo = response1.data.courses.filter((course) => {
+    const coursesFilteredBycourseNo = response1.data.courses.filter((course) => {
       return course.courseNo === courseNo;
     });
-    
+
     const coursesFilteredBycurriculum = coursesFilteredBycourseNo.filter((course) => {
       return course.curriculum == curriculum;
     });
     const csoList = coursesFilteredBycurriculum[0].csoList;
-    // Initialize an array to store sections data
 
-    // const sectionsData = [];
-    // const relevantCourse = [];
-    // // Iterate over each course number
-    // for (let i = 0; i < courseNoList.length; i++) {
+    res.status(200).json({ csoList, soList });
 
-    //   const courseNo = courseNoList[i];
-    //   // Fetch section data from the second API endpoint
-    //   const response = await axios.get('https://api.cpe.eng.cmu.ac.th/api/v1/course/sections?courseNo=' + `${courseNo}` + '&year=' + `${year}` + '&semester=' + `${semester}`, config_2);
-    //   const tempdatabese = await Coursess.find({ courseNo: courseNo, year: year, semester: semester }).exec();
-
-    //   // Process sections data
-    //   const sections =  response.data.sections.map((section) => {
-    //     const coTeachers = section.coTeachers || null;
-    //     const coTeacherNames = coTeachers.map((coTeacher) => {
-    //       return {
-    //         // Check if type is 'ExternalTeacher', show fullName; else, show firstNameTH and firstNameEN
-    //         type: coTeacher.type,
-    //         fullName: coTeacher.type === 'ExternalTeacher' ? coTeacher.fullName : undefined,
-    //         NameTH: coTeacher.type !== 'ExternalTeacher' ? coTeacher.firstNameTH + " " + coTeacher.lastNameTH : undefined,
-    //         NameEN: coTeacher.type !== 'ExternalTeacher' ? coTeacher.firstNameEN + " " + coTeacher.lastNameEN : undefined,
-    //       };
-    //     });
-
-    //     const courseTitleEN = response1.data.courses.find((course) => course.courseNo === courseNo)?.courseTitleEN;
-    //     const courseTitleTH = response1.data.courses.find((course) => course.courseNo === courseNo)?.courseTitleTH;
-
-    //     let statuss = "fail"; // Default status if no sectionNumber equals "1"
-    //     let csoList = null;  // Initialize csoList to a default value (null) or an appropriate default based on your use case.
-    //     function findCourse(courseNo) {
-    //       return response1.data.courses.find((course) => {
-    //         return course.courseNo === courseNo;
-    //       });
-    //     }
-
-        
-
-
-
-    
-    //       const foundCourse = findCourse(courseNo);
-    //     if (foundCourse) {
-    //        csoList = foundCourse.csoList;
-    //        csoList.forEach((csoItem) => {
-    //         csoItem.Csoscore = 0;
-    //       });
-    //     }
-        
-    //       const a = {
-    //         courseNo: courseNo,
-    //         year: year,
-    //         semester: semester,
-    //         csoList:csoList,
-    //         section: [
-    //           {
-    //             sectionNumber: section.section,
-    //             status: "In Progress"
-    //           }
-    //         ]
-    //       };
-      
-    //       if (tempdatabese.length > 0) {
-    //         // At least one course found
-    //         //const tempdatabese = tempdatabese[0];
-    //         const sectionNumbers = tempdatabese[0].section.map(section => section.sectionNumber);
-    //         const allstatus = tempdatabese[0].section.map(section => section.status);
-    //         let sectionAsString = section.section.toString();
-      
-    //         for (let i = 0; i < tempdatabese[0].section.length; i++) {
-    //           if (sectionNumbers[i] === sectionAsString) {
-    //             statuss = allstatus[i];
-    //             break; // Found a section with sectionNumber === "1", exit the loop
-    //           }
-    //         }
-      
-    //         if (statuss == "fail") {
-    //             // Add the new section to the course
-    //             tempdatabese[0].section.push({
-    //               sectionNumber: sectionAsString,
-    //               status: "In Progress"
-    //             });
-    //             statuss = "In Progress";
-    //         }
-    //       } else {
-    //         // No courses found, so create a new one
-    //         statuss = "In Progress";
-    //         tempdatabese.push(a);
-    //       }
-      
-
-
-
-
-
-
-
-
-
-
-    //     if (emailPrefix === section.teacher.firstNameEN.toLowerCase()) {
-    //       instructorName = section.teacher.firstNameEN + " " + section.teacher.lastNameEN;
-    //       return {
-    //         courseNo: courseNo,
-    //         sectionNo: [section.section],
-    //         courseTitleTH: courseTitleTH,
-    //         courseTitleEN: courseTitleEN,
-    //         NameTH: section.teacher.firstNameTH + " " + section.teacher.lastNameTH,
-    //         NameEN: section.teacher.firstNameEN + " " + section.teacher.lastNameEN,
-    //         coTeachers: coTeachers.length > 0 ? coTeacherNames : null,
-    //         status: statuss,
-    //       };
-    //     } else {
-    //       const relevant = {
-    //         courseNo: courseNo,
-    //         section: section.section,
-    //         courseTitleTH: courseTitleTH,
-    //         courseTitleEN: courseTitleEN,
-    //         instructorName: section.teacher.firstNameEN + " " + section.teacher.lastNameEN,
-    //         coTeachers: coTeachers.length > 0 ? coTeacherNames : null
-    //       };
-    //       relevantCourse.push(relevant);
-    //       return null; // Skip this section if the condition is not met
-    //     }
-    //   });
-
-    //   // Filter out sections that are null (didn't meet the condition)
-    //   const validSections = sections.filter((section) => section !== null);
-
-    //   // Group sections with the same courseNo, NameEN, and similar coTeachers
-    //   validSections.forEach((section) => {
-    //     const existingSection = sectionsData.find((s) =>
-    //       s.courseNo === section.courseNo &&
-    //       s.NameEN === section.NameEN &&
-    //       JSON.stringify(s.coTeachers) === JSON.stringify(section.coTeachers)
-    //     );
-
-    //     if (existingSection) {
-    //       existingSection.sectionNo.push(section.sectionNo);
-    //     } else {
-    //       section.sectionNo = [section.sectionNo];
-    //       sectionsData.push(section);
-    //     }
-    //   });
-    //   console.log(tempdatabese);
-    //    const save = new Coursess(tempdatabese[0]);
-    //    console.log(save);
-    //   save.save();
-    // }
-
-    // // Convert sectionNo arrays to comma-separated strings
-    // sectionsData.forEach((section) => {
-    //   section.sectionNo = section.sectionNo.join([', ']);
-    // });
-
-    // sectionsData.sort((a, b) => a.courseNo.localeCompare(b.courseNo));
-    // sectionsData.sectionNo = [sectionsData.sectionNo ];
-
-    res.status(200).json({csoList,soList});
- 
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 })
 
+router.get('/csoScore', async (req, res) => {
+  try {
+    // Get parameters from the request query
+    const courseNo = req.query.courseNo;
+    const year = req.query.year;
+    const semester = req.query.semester;
+    const section = req.query.section;
+    const scoreUsesList = req.body.scoreUsesList;
+    const score = req.body.score;
+    const standard = req.body.standard;
+
+    const tempdatabese = await Coursess.find({ courseNo: courseNo, year: year, semester: semester }).exec();
+
+
+    for (let i = 0; i < scoreUsesList.length; i++) {
+      if (!tempdatabese[0].csoList[i]) {
+        // If csoList[i] does not exist, initialize it as an object
+        tempdatabese[0].csoList[i] = {};
+      }
+      if (!tempdatabese[0].csoList[i].scoreUsesList) {
+        // If scoreUsesList does not exist, initialize it as an empty array
+        tempdatabese[0].csoList[i].scoreUsesList = [];
+      }
+      if (tempdatabese[0].csoList[i].scoreUsesList.length == 0) {
+        tempdatabese[0].csoList[i].scoreUsesList.push(scoreUsesList[i]);
+      }
+    }
+    const save = new Coursess(tempdatabese[0]);
+    save.save();
+    const tempsec = tempdatabese[0].section.find((sec) => sec.sectionNumber === section);
+
+    const NumberPeoplei = [];
+    for (let i = 0; i < score.length; i++) {
+      const NumberPeoplej = [];
+      for (let j = 0; j < score[i].length; j++) {
+        let NumberPeoplek = [0, 0, 0, 0, 0];
+        for (let k = 0; k < score[i][j].length; k++) {
+          if (score[i][j][k] <= standard[i][j][0]) {
+            NumberPeoplek[0]++;
+          }
+          else if (score[i][j][k] <= standard[i][j][1]) {
+            NumberPeoplek[1]++;
+          }
+          else if (score[i][j][k] <= standard[i][j][2]) {
+            NumberPeoplek[2]++;
+          }
+          else if (score[i][j][k] <= standard[i][j][3]) {
+            NumberPeoplek[3]++;
+          }
+          else {
+            NumberPeoplek[4]++;
+          }
+        }
+        NumberPeoplej.push(NumberPeoplek);
+      }
+      NumberPeoplei.push(NumberPeoplej);
+    }
+    
+    const csoavgeach =[];
+    const csoavg =[];
+    for (let i = 0; i < NumberPeoplei.length; i++) {
+      for (let j = 0; j < NumberPeoplei[i].length; j++) {
+        for (let k = 0; k < NumberPeoplei[i].length; k++) {
+
+        }
+      }
+
+    }
+
+
+
+
+    // Fetch course data from the first API endpoint
+
+    res.status(200).json(NumberPeoplei);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
 
 module.exports = router
+
+
+
+
