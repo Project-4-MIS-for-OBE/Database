dfbec6e18f5ff1e2209276ef4c3b5502490d416f
buboobear <jedsa.pd@gmail.com>
2023-10-12 01:24:52 +0700

    Revert "make deployment and debugging"
    This reverts commit dae0a299945d8dcba751e601991762aeafe39307.

--- app.js
+++ app.js
@@ -20,18 +20,18 @@ mongoose
 	.then(() => console.log("Connection Successfully!!!"))
 	.catch((err) => console.error(err));
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
